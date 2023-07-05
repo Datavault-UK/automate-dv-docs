0e9e0a93670e7c3d3ded469bd9ec1e46d6fa1ba1
Alex Higgs <alex.higgs@data-vault.com>
2023-07-05 10:38:26 +0000

    Generating models with the ADV CLI!

--- docs_snippets/models/primed_stage/stg_customer.sql
+++ docs_snippets/models/primed_stage/stg_customer.sql
@@ -1,18 +1,15 @@
 {%- set yaml_metadata -%}
 source_model: CUSTOMER
-hashed_columns:
-  CUSTOMER_HK: c_custkey
 derived_columns:
   CUSTOMER_ID: c_custkey
-  LOAD_DATETIME: "!1998-01-01"
-  RECORD_SOURCE: "!TPCH_CUSTOMER"
+  LOAD_DATETIME: '!1998-01-01'
+  RECORD_SOURCE: '!TPCH_CUSTOMER'
+hashed_columns:
+  CUSTOMER_HK: c_custkey
 {%- endset -%}
 
 {% set metadata_dict = fromyaml(yaml_metadata) %}
 
-{{ dbtvault.stage(include_source_columns=true,
-                  source_model=metadata_dict["source_model"],
-                  derived_columns=metadata_dict["derived_columns"],
-                  null_columns=metadata_dict["null_columns"],
-                  hashed_columns=metadata_dict["hashed_columns"],
-                  ranked_columns=metadata_dict["ranked_columns"]) }}
+{{ automate_dv.stage(source_model=metadata_dict["source_model"],
+                     derived_columns=metadata_dict["derived_columns"],
+                     hashed_columns=metadata_dict["hashed_columns"]) }}
--- docs_snippets/models/raw_vault/hubs/hub_orders_multi_source.sql
+++ docs_snippets/models/raw_vault/hubs/hub_orders_multi_source.sql
@@ -1,19 +1,11 @@
-{{ config(full_refresh = true) }}
+{%- set source_model = ["stg_customer", "stg_orders"] -%}
+{%- set src_pk = "CUSTOMER_HK" -%}
+{%- set src_nk = "CUSTOMER_ID" -%}
+{%- set src_ldts = "LOAD_DATETIME" -%}
+{%- set src_source = "RECORD_SOURCE" -%}
 
-{%- set yaml_metadata -%}
-source_model:
-  - stg_customer
-  - stg_orders
-src_pk: CUSTOMER_HK
-src_nk: CUSTOMER_ID
-src_ldts: LOAD_DATETIME
-src_source: RECORD_SOURCE
-{%- endset -%}
-
-{% set metadata_dict = fromyaml(yaml_metadata) %}
-
-{{ dbtvault.hub(src_pk=metadata_dict["src_pk"],
-                src_nk=metadata_dict["src_nk"],
-                src_ldts=metadata_dict["src_ldts"],
-                src_source=metadata_dict["src_source"],
-                source_model=metadata_dict["source_model"]) }}
+{{ automate_dv.hub(source_model=source_model,
+                   src_pk=src_pk,
+                   src_nk=src_nk,
+                   src_ldts=src_ldts,
+                   src_source=src_source) }}
